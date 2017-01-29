7cc57f1c0a01f8e0db7e315759636c4acc605d7a
egglestn <segglestn@gmail.com>
2017-01-29 15:51:48 +0000

    Answers index, add font awesome

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129110547) do
+ActiveRecord::Schema.define(version: 20170129153424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170129110547) do
     t.datetime "updated_at", null: false
     t.integer  "prev"
     t.integer  "next"
+    t.integer  "section"
     t.index ["answers_id"], name: "index_questions_on_answers_id", using: :btree
   end
 
