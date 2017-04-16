b17bfaae9a8fa27d40c3cbdadabf3755d10a088b
egglestn <segglestn@gmail.com>
2017-04-16 16:16:23 +0100

    Make answer display dynamic

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -1,54 +1,16 @@
 $(document).ready(function() {
   $('#content_category').change(function(event) {
-    // TODO why do we need this??
-    console.log("");
-
     var $content = $('.content');
-    var $leftContent = $content.find(".left");
-    var $options = $content.find(".optiondiv");
-    var $score = $content.find(".score");
+    var $answers = $content.find(".answers");
 
     // Hard-coded value, needs to match the "Question" option in the category enum in content.rb
     if (event.target.options.selectedIndex == 1)
     {
-       //console.log("Set to question");
-       $options.each(function() {
-         this.classList.remove("hidden");
-       });
-       $score.hide();
+       $answers.hide();
     }
     else {
       //console.log("Not a question");
-      var first = true;
-      $options.each(function() {
-         if (first) {
-           first = false
-         }
-         else {
-           this.classList.add("hidden");
-         }
-      });
-      $score.show();
+      $answers.show();
     }
   });
-
-  function createAnswer(i, answers) {
-    var ansDiv = document.createElement("div");
-    var ansLabel = document.createElement("label");
-    ansLabel.textContent = "Answer " + (i+1);
-    ansDiv.append(ansLabel);
-
-    var ansSelect = document.createElement("select");
-
-    answers.forEach(function(ansContent) {
-      ansOption = document.createElement("option");
-      ansOption.value = ansContent.id;
-      ansOption.innerHTML = ansContent.key;
-      ansSelect.append(ansOption);
-    });
-
-    ansDiv.append(ansSelect);
-
-    return ansDiv;
-  }
 });
