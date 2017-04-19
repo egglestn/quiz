1ffdac5a69196018086f8bcb287f66a555def420
egglestn <segglestn@gmail.com>
2017-04-19 19:37:02 +0100

    Attempt 3

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,6 +1,4 @@
 Ckeditor.setup do |config|
   config.assets_languages = ['en']
   config.assets_plugins = []
-  Rails.application.config.assets.paths += %w( ckeditor/config.js )
-  Rails.application.config.assets.precompile += %w( ckeditor/* )
 end
