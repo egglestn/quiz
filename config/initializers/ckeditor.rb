851070bca07bc96357b6ed46565d00e537f8649f
egglestn <segglestn@gmail.com>
2017-04-19 19:35:09 +0100

    Second attempt

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,6 +1,6 @@
 Ckeditor.setup do |config|
   config.assets_languages = ['en']
   config.assets_plugins = []
-  # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  #config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
+  Rails.application.config.assets.paths += %w( ckeditor/config.js )
+  Rails.application.config.assets.precompile += %w( ckeditor/* )
 end
