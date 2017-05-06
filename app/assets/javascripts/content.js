009c02823d2ede09be2fe37013dbc888da21cea4
egglestn <segglestn@gmail.com>
2017-05-06 14:55:45 +0100

    Hide answers for explanation

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#content_category').change(function(event) {
     var $content = $('.content');
     var $answers = $content.find(".answers");
-    console.log("");
+    console.log(".");
 
     // Hard-coded value, needs to match the "Question" option in the category enum in content.rb
     if (event.target.options.selectedIndex == 1)
@@ -10,7 +10,6 @@ $(document).ready(function() {
        $answers.hide();
     }
     else {
-      //console.log("Not a question");
       $answers.show();
     }
   });
