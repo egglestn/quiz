62785c9865cacb159ad6a70ca13f63104954d8e0
egglestn <segglestn@gmail.com>
2017-06-24 09:57:14 +0100

    Add footnotes to DB and form for content

---
+++ db/migrate/20170624084747_add_footnotes_to_contents.rb
@@ -0,0 +1,5 @@
+class AddFootnotesToContents < ActiveRecord::Migration[5.0]
+  def change
+    add_column :contents, :footnotes, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,62 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_417_181_838) do
+ActiveRecord::Schema.define(version: 20170624084747) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'answers', force: :cascade do |t|
-    t.string   'text'
-    t.integer  'score'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer  'content_id'
-    t.index ['content_id'], name: 'index_answers_on_content_id', using: :btree
+  create_table "answers", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "content_id"
+    t.index ["content_id"], name: "index_answers_on_content_id", using: :btree
   end
 
-  create_table 'contents', force: :cascade do |t|
-    t.string   'text'
-    t.string   'key'
-    t.integer  'section'
-    t.integer  'content_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer  'category'
-    t.integer  'next_id'
-    t.index ['content_id'], name: 'index_contents_on_content_id', using: :btree
-    t.index ['next_id'], name: 'index_contents_on_next_id', using: :btree
+  create_table "contents", force: :cascade do |t|
+    t.string   "text"
+    t.string   "key"
+    t.integer  "section"
+    t.integer  "content_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "category"
+    t.integer  "next_id"
+    t.string   "footnotes"
+    t.index ["content_id"], name: "index_contents_on_content_id", using: :btree
+    t.index ["next_id"], name: "index_contents_on_next_id", using: :btree
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string   'text'
-    t.integer  'answers_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer  'prev'
-    t.integer  'next'
-    t.integer  'section'
-    t.index ['answers_id'], name: 'index_questions_on_answers_id', using: :btree
+  create_table "questions", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "answers_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "prev"
+    t.integer  "next"
+    t.integer  "section"
+    t.index ["answers_id"], name: "index_questions_on_answers_id", using: :btree
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.inet     'current_sign_in_ip'
-    t.inet     'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.integer  'role', default: 0
-    t.index ['email'], name: 'index_users_on_email', unique: true, using: :btree
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "role",                   default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key 'answers', 'contents'
-  add_foreign_key 'contents', 'contents'
-  add_foreign_key 'questions', 'answers', column: 'answers_id'
+  add_foreign_key "answers", "contents"
+  add_foreign_key "contents", "contents"
+  add_foreign_key "questions", "answers", column: "answers_id"
 end
